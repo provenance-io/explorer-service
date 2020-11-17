be802dcb68437f5999b19cefe7ac0cb22562e488
Carlton Hanna <channa@figure.com>
2020-11-17 15:29:32 -0700

    Offset paging index in controller by +1 for frontend calls

--- service/src/main/kotlin/io/provenance/explorer/web/ExplorerController.kt
+++ service/src/main/kotlin/io/provenance/explorer/web/ExplorerController.kt
@@ -17,8 +17,8 @@ class ExplorerController(private val serviceProperties: ServiceProperties,
     @ApiOperation(value = "Return the latest block transactions")
     @GetMapping(value = ["/recent/txs"],
             produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun txsRecent(@RequestParam(required = false, defaultValue = "10") count: Int, @RequestParam(required = false, defaultValue = "0") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String = "desc"):
-            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentTransactions(count, page, sort))
+    fun txsRecent(@RequestParam(required = false, defaultValue = "10") count: Int, @RequestParam(required = false, defaultValue = "1") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String = "desc"):
+            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentTransactions(count, page - 1, sort))
 
     @ApiOperation(value = "Return block at specified height")
     @GetMapping(value = ["/block"],
@@ -35,14 +35,14 @@ class ExplorerController(private val serviceProperties: ServiceProperties,
     @ApiOperation(value = "Returns most recent blocks")
     @GetMapping(value = ["/recent/blocks"],
             produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun recentBlocks(@RequestParam(required = true, defaultValue = "10") count: Int, @RequestParam(required = true, defaultValue = "0") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String):
-            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentBlocks(count, page, sort))
+    fun recentBlocks(@RequestParam(required = true, defaultValue = "10") count: Int, @RequestParam(required = true, defaultValue = "1") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String):
+            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentBlocks(count, page - 1, sort))
 
     @ApiOperation(value = "Returns recent validators")
     @GetMapping(value = ["/recent/validators"],
             produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun validators(@RequestParam(required = false, defaultValue = "10") count: Int, @RequestParam(required = false, defaultValue = "0") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String):
-            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentValidators(count, page, sort))
+    fun validators(@RequestParam(required = false, defaultValue = "10") count: Int, @RequestParam(required = false, defaultValue = "1") page: Int, @RequestParam(required = false, defaultValue = "desc") sort: String):
+            ResponseEntity<Any> = ResponseEntity.ok(explorerService.getRecentValidators(count, page - 1, sort))
 
     @ApiOperation(value = "Returns set of validators at block height")
     @GetMapping(value = ["/validators"],
