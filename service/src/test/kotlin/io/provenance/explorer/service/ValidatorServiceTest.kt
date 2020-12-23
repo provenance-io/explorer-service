130a90b954ade3efa6c1566032717f12adeafd2f
Carlton Hanna <channa@figure.com>
2020-12-23 12:21:27 -0700

    Fix test references

--- service/src/test/kotlin/io/provenance/explorer/service/ValidatorServiceTest.kt
+++ service/src/test/kotlin/io/provenance/explorer/service/ValidatorServiceTest.kt
@@ -15,35 +15,35 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @Transactional
-open class ValidatorAddressServiceTest{
+open class ValidatorServiceTest{
 
     @Autowired
-    lateinit var validatorAddressService: ValidatorAddressService
+    lateinit var validatorService: ValidatorService
 
 
     @Test(expected=KotlinNullPointerException::class)
     @Ignore("TODO turn into integration tests")
     fun `should throw KotlinNullPointerException error when searching with invalid consensus address`() {
-        validatorAddressService.findAddressesByConsensusAddress("I'm not match worthy")
+        validatorService.findAddressesByConsensusAddress("I'm not match worthy")
     }
 
     @Test(expected=KotlinNullPointerException::class)
     @Ignore("TODO turn into integration tests")
     fun `should throw KotlinNullPointerException error when searching with invalid consensus pub key address`() {
-        validatorAddressService.findAddressesByConsensusPubKeyAddress("I'm not match worthy either")
+        validatorService.findAddressesByConsensusPubKeyAddress("I'm not match worthy either")
     }
 
     @Test
     @Ignore("TODO turn into integration tests")
     fun `should return matching operator address from consesus address`(){
-        val result = validatorAddressService.findAddressesByConsensusAddress("tpvalcons1pkj6cy0a6acmm3q2augsgkj27h79k6ywqj35gu")
+        val result = validatorService.findAddressesByConsensusAddress("tpvalcons1pkj6cy0a6acmm3q2augsgkj27h79k6ywqj35gu")
         Assert.assertEquals("tpvaloper14neg0whj7puhwks6536a8lqp7msvd9p028p7jm", result)
     }
 
     @Test
     @Ignore("TODO turn into integration tests")
     fun `should return matching operator address from consesus pubkey address`(){
-        val result = validatorAddressService.findAddressesByConsensusPubKeyAddress("tpvalconspub1zcjduepq2hs3lwp8d0u4j0389knqaer4j97quc8v9updrz4yecgkm3uy46ls6rm7ph")
+        val result = validatorService.findAddressesByConsensusPubKeyAddress("tpvalconspub1zcjduepq2hs3lwp8d0u4j0389knqaer4j97quc8v9updrz4yecgkm3uy46ls6rm7ph")
         Assert.assertEquals("tpvaloper1y3n8quzwm480v3yqsjwezplrj2ts0s9wlyfwup", result)
     }
 }
