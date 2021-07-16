903c04a3b0a85dc8ed1654ed191e19e1644020f6
Ergels Gaxhaj <egaxhaj@figure.com>
2021-07-16 15:36:23 -0500

    Implemented a solution for tx printed as base64 string (#145)

--- service/src/main/kotlin/io/provenance/explorer/Application.kt
+++ service/src/main/kotlin/io/provenance/explorer/Application.kt
@@ -2,6 +2,7 @@ package io.provenance.explorer
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import io.provenance.explorer.config.ExplorerProperties
 import io.provenance.explorer.domain.extensions.configureProvenance
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
@@ -30,4 +31,7 @@ fun main(args: Array<String>) {
 val OBJECT_MAPPER = ObjectMapper()
     .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
     .configureProvenance()
+
+/* Singleton instance that can safely be shared globally */
+val JSON_NODE_FACTORY: JsonNodeFactory = JsonNodeFactory.instance
 
