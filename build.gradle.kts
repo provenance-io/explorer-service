121e005e8ed85f346364b857c3aa84bb29f9bb00
Peter van Rensburg <pvanrensburg@figure.com>
2022-01-12 12:00:30 -0700

    fix build and rev gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ subprojects {
     repositories {
         mavenLocal()
         mavenCentral()
-        jcenter()
         maven { url = project.uri("https://maven.java.net/content/groups/public") }
         maven { url = project.uri("https://oss.sonatype.org/content/repositories/snapshots") }
         maven { url = project.uri("https://dl.bintray.com/kotlin/exposed") }
