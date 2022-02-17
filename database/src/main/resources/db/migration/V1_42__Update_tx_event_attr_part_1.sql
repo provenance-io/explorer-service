15419f2a79c76550fac8e8470539689bae543c44
Carolyn Russell <crussell@figure.com>
2022-02-16 17:31:04 -0700

    Hotfix - Fixing migrations to work on an empty DB

--- database/src/main/resources/db/migration/V1_42__Update_tx_event_attr_part_1.sql
+++ database/src/main/resources/db/migration/V1_42__Update_tx_event_attr_part_1.sql
@@ -7,18 +7,21 @@ $$
     BEGIN
         SELECT array_agg(DISTINCT tx_msg_event_id) FROM tx_msg_event_attr INTO msg_ids;
 
-        FOREACH msg_id IN ARRAY msg_ids
-            LOOP
-                UPDATE tx_msg_event_attr
-                SET attr_idx = t.rn - 1
-                FROM (
-                         SELECT id,
-                                tx_msg_event_id,
-                                row_number() OVER (PARTITION BY tx_msg_event_id ORDER BY id) AS rn
-                         FROM tx_msg_event_attr
-                         WHERE tx_msg_event_attr.tx_msg_event_id = msg_id
-                     ) t
-                WHERE t.id = tx_msg_event_attr.id;
-            END LOOP;
+        IF msg_ids IS NOT NULL
+        THEN
+            FOREACH msg_id IN ARRAY msg_ids
+                LOOP
+                    UPDATE tx_msg_event_attr
+                    SET attr_idx = t.rn - 1
+                    FROM (
+                             SELECT id,
+                                    tx_msg_event_id,
+                                    row_number() OVER (PARTITION BY tx_msg_event_id ORDER BY id) AS rn
+                             FROM tx_msg_event_attr
+                             WHERE tx_msg_event_attr.tx_msg_event_id = msg_id
+                         ) t
+                    WHERE t.id = tx_msg_event_attr.id;
+                END LOOP;
+        END IF;
     END
 $$;
--- database/src/main/resources/db/migration/V1_43__Update_tx_event_attr_part_2.sql
+++ database/src/main/resources/db/migration/V1_43__Update_tx_event_attr_part_2.sql
@@ -1,5 +1,5 @@
 SELECT 'Update tx_msg_event_attr.attr_hash' AS comment;
--- CREATE EXTENSION IF NOT EXISTS pgcrypto; -- CREATED MANUALLY DUE TO PERMISSIONS ERROR
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 UPDATE tx_msg_event_attr
 SET attr_hash = encode(digest(concat(attr_idx,attr_key,attr_value)::text, 'sha512'::TEXT), 'base64'::TEXT)
