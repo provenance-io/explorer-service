88713d78b2ddc3fa16e50f3966e295afab7bc44a
Carolyn Russell <cj.russell7601@gmail.com>
2022-10-14 10:46:31 -0600

    Moving from BearerToken to Authorization for Cors reasons

--- service/src/main/kotlin/io/provenance/explorer/config/interceptor/JwtInterceptor.kt
+++ service/src/main/kotlin/io/provenance/explorer/config/interceptor/JwtInterceptor.kt
@@ -3,6 +3,7 @@ package io.provenance.explorer.config.interceptor
 import io.provenance.explorer.OBJECT_MAPPER
 import io.provenance.explorer.domain.exceptions.InvalidJwtException
 import io.provenance.explorer.domain.extensions.fromBase64
+import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -19,7 +20,7 @@ class JwtInterceptor : HandlerInterceptor {
         response: HttpServletResponse,
         handler: Any
     ): Boolean {
-        val jwt = request.getHeader("BearerToken")?.removePrefix("Bearer")?.trimStart()
+        val jwt = request.getHeader(HttpHeaders.AUTHORIZATION)?.removePrefix("Bearer")?.trimStart()
 
         if (!jwt.isNullOrBlank()) {
             try {
