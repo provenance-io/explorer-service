e1673a47f525d4b72ddee0a97e17bb3ef0d04424
Carlton N Hanna <nullpointer0x00@gmail.com>
2023-11-09 08:05:09 -0700

    start adding tests for toObjectNode that is problematic with new memo with json

---
+++ service/src/test/kotlin/io/provenance/explorer/domain/extensions/ExtenstionsKtTest.kt
@@ -0,0 +1,32 @@
+package io.provenance.explorer.domain.extensions
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.apache.commons.lang3.StringEscapeUtils
+
+class ExtensionsKtTest {
+
+    @Test
+    fun toObjectNode() {
+//        // Given an input JSON string
+//        val inputJson = "{\"key\":\"value\"}"
+//
+//        // Create an ObjectMapper for testing
+//        val objectMapper = ObjectMapper()
+//
+//        // Escape the input JSON
+//        val escapedInput = StringEscapeUtils.escapeJson(inputJson)
+//
+//        // Convert the escaped JSON to an ObjectNode
+//        val resultObjectNode = escapedInput.toObjectNode(objectMapper)
+//
+//        // Create an expected ObjectNode
+//        val expectedObjectNode = objectMapper.createObjectNode()
+//        expectedObjectNode.put("key", "value")
+//
+//        // Assert that the result ObjectNode is equal to the expected ObjectNode
+//        assertEquals(expectedObjectNode, resultObjectNode)
+    }
+}
