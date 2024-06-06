ac53e86844c5bb5e9bff095933bb16fc5228b2ee
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-06-06 10:30:39 -0600

    remove unneeded tags

--- service/src/test/kotlin/io/provenance/explorer/domain/ExtensionsTest.kt
+++ service/src/test/kotlin/io/provenance/explorer/domain/ExtensionsTest.kt
@@ -11,7 +11,6 @@ import java.util.Base64
 class ExtensionsTest {
 
     @Test
-    @Tag("junit-jupiter")
     fun `should return bech32 address from given secp256k1 pubkey proto`() {
         val bech32address = "tp102789pey8hyd25pzvxhxn8k62nrffcjtng8kef"
         val key = Base64.getDecoder().decode("Ar2rEV/s88aWyU8CMX+ipwL7/XNI5Z6gexrWv6n1Dxx6")
@@ -21,7 +20,6 @@ class ExtensionsTest {
     }
 
     @Test
-    @Tag("junit-jupiter")
     fun `should return bech32 address from given secp256r1 pubkey proto`() {
         val bech32address = "cosmos1g95vqmg5fdlgm0ccyrwd0wvm6axgwkcrn5n0pl3llptr6qz8aayqtzqj47"
         val key = Base64.getDecoder().decode("Angqd50SIljag/sk8T+2eo/7vLyAvodInF8E3SKDdnmN")
