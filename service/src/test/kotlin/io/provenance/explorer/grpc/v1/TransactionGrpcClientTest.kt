625bea3988f5787843d2cf7cf2fbd5bae7a93079
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-07-09 10:54:55 -0600

    update tx grpc query to use new structure

---
+++ service/src/test/kotlin/io/provenance/explorer/grpc/v1/TransactionGrpcClientTest.kt
@@ -0,0 +1,33 @@
+package io.provenance.explorer.grpc.v1
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import java.net.URI
+import cosmos.tx.v1beta1.ServiceOuterClass
+import io.provenance.explorer.domain.exceptions.TendermintApiException
+import org.junit.jupiter.api.Disabled
+
+class TransactionGrpcClientTest {
+
+    @Test
+    @Disabled("Test was used to manually call the endpoint")
+    fun `test getTxsByHeight success`() = runBlocking {
+        val uri = URI("grpcs://grpc.test.provenance.io:443")
+        val transactionGrpcClient = TransactionGrpcClient(uri)
+
+        try {
+            val height = 23440770
+            val total = 1
+
+            val txResponses: List<ServiceOuterClass.GetTxResponse> = transactionGrpcClient.getTxsByHeight(height, total)
+
+            txResponses.forEach { txResponse ->
+                println("TxResponse: ${txResponse.txResponse}")
+                println("Tx: ${txResponse.tx}")
+            }
+        } catch (e: TendermintApiException) {
+            println("Error occurred: ${e.message}")
+        }
+    }
+
+}
