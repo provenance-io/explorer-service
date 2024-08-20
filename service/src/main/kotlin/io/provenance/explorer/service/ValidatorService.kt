d94275632bde82a9dc603ab39d3c393497779c77
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-08-20 11:07:56 -0600

    change type to long to match the grpc type

--- service/src/main/kotlin/io/provenance/explorer/service/ValidatorService.kt
+++ service/src/main/kotlin/io/provenance/explorer/service/ValidatorService.kt
@@ -334,7 +334,7 @@ class ValidatorService(
             moniker = stakingVal.json.description.moniker,
             addressId = stakingVal.operatorAddress,
             consensusAddress = stakingVal.consensusAddr,
-            proposerPriority = validator?.proposerPriority?.toBigInteger(),
+            proposerPriority = validator?.proposerPriority,
             votingPower = if (validator != null) {
                 CountTotal(
                     validator.votingPower.toBigInteger(),
