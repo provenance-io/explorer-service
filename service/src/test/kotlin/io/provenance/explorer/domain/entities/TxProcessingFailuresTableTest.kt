b1b83ad004a57bc17a07edc679d5d35d45559196
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-08-26 07:54:27 -0600

    update tests

--- service/src/test/kotlin/io/provenance/explorer/domain/entities/TxProcessingFailuresTableTest.kt
+++ service/src/test/kotlin/io/provenance/explorer/domain/entities/TxProcessingFailuresTableTest.kt
@@ -3,17 +3,21 @@ import io.provenance.explorer.domain.entities.TxProcessingFailuresTable
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.Database
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
 class TxProcessingFailureRecordTest {
 
     @Test
-    fun testInsertOrUpdate_newRecord() {
+    fun `test tx_processing_failures table insertOrUpdate`() {
         Database.connect("jdbc:h2:mem:test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;", driver = "org.h2.Driver")
+
         transaction {
             val sql = this::class.java.getResource("/db/migration/V1_91__Create_tx_processing_failures_table.sql")!!.readText()
             exec(sql)
         }
+
         transaction {
             TxProcessingFailureRecord.insertOrUpdate(
                 blockHeight = 100,
@@ -23,39 +27,41 @@ class TxProcessingFailureRecordTest {
                 success = false
             )
 
+            var record = TxProcessingFailureRecord.find {
+                (TxProcessingFailuresTable.blockHeight eq 100) and
+                        (TxProcessingFailuresTable.txHash eq "testHash") and
+                        (TxProcessingFailuresTable.processType eq "testProcess")
+            }.firstOrNull()
+
+            assertNotNull(record, "Record should not be null")
+            assertEquals(100, record?.blockHeight)
+            assertEquals("testHash", record?.txHash)
+            assertEquals("testProcess", record?.processType)
+            assertEquals("testError", record?.errorMessage)
+            assertEquals(false, record?.success)
+        }
+
+        transaction {
+            TxProcessingFailureRecord.insertOrUpdate(
+                blockHeight = 100,
+                txHash = "testHash",
+                processType = "testProcess",
+                errorMessage = "updatedError",
+                success = true
+            )
+
             val record = TxProcessingFailureRecord.find {
                 (TxProcessingFailuresTable.blockHeight eq 100) and
                         (TxProcessingFailuresTable.txHash eq "testHash") and
                         (TxProcessingFailuresTable.processType eq "testProcess")
             }.firstOrNull()
+
+            assertNotNull(record, "Record should not be null")
+            assertEquals(100, record?.blockHeight)
+            assertEquals("testHash", record?.txHash)
+            assertEquals("testProcess", record?.processType)
+            assertEquals("updatedError", record?.errorMessage)
+            assertEquals(true, record?.success)
         }
     }
-
-//    @Test
-//    fun testInsertOrUpdate_updateRecord() {
-//        transaction {
-//            TxProcessingFailureRecord.insertOrUpdate(
-//                blockHeight = 100,
-//                txHash = "testHash",
-//                processType = "testProcess",
-//                errorMessage = "testError",
-//                success = false
-//            )
-//
-//            TxProcessingFailureRecord.insertOrUpdate(
-//                blockHeight = 100,
-//                txHash = "testHash",
-//                processType = "testProcess",
-//                errorMessage = "updatedError",
-//                success = true
-//            )
-//
-//            val record = TxProcessingFailureRecord.find {
-//                (TxProcessingFailuresTable.blockHeight eq 100) and
-//                        (TxProcessingFailuresTable.txHash eq "testHash") and
-//                        (TxProcessingFailuresTable.processType eq "testProcess")
-//            }.firstOrNull()
-//
-//        }
-//    }
 }
