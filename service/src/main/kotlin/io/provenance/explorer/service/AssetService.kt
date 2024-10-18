7c2f1abb7639895cabc265be561493e00260a8c2
Carlton N Hanna <nullpointer0x00@gmail.com>
2024-10-18 08:50:29 -0600

    remove logging

--- service/src/main/kotlin/io/provenance/explorer/service/AssetService.kt
+++ service/src/main/kotlin/io/provenance/explorer/service/AssetService.kt
@@ -250,7 +250,6 @@ class AssetService(
             includeScopes = false,
             fromDate = assetPricinglastRun?.toDateTime()
         )
-        logger.info("updating latest prices size ${latestPrices.size}")
         latestPrices.forEach { price ->
             if (price.denom != UTILITY_TOKEN) {
                 val marker = getAssetRaw(price.denom)
