84d8de8c54f8bf5f5c53d97c45225cee700b2e74
Matt Conroy <matt@figure.com>
2024-12-31 10:00:21 -0500

    create/ignore or replace to avoid conflicts if we shove this into the db sooner.

--- database/src/main/resources/db/migration/V1_98__Tx_cache_counter_trigger.sql
+++ database/src/main/resources/db/migration/V1_98__Tx_cache_counter_trigger.sql
@@ -1,11 +1,10 @@
-CREATE TABLE row_counts
+CREATE TABLE IF NOT EXISTS row_counts
 (
     id            VARCHAR(20) NOT NULL PRIMARY KEY,
     current_count BIGINT      NOT NULL
 );
 
-CREATE
-OR REPLACE FUNCTION row_counts_trigger()
+CREATE OR REPLACE FUNCTION tx_cache_row_counts_trigger()
 RETURNS TRIGGER
 LANGUAGE PLPGSQL
 AS
@@ -25,8 +24,22 @@ BEGIN
 END;
 $$;
 
+    -- Create the trigger only if it does not already exist
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_trigger
+        WHERE tgname = 'row_counts_trigger'
+    ) THEN
 CREATE TRIGGER row_counts_trigger
     BEFORE INSERT OR DELETE
 ON tx_cache
-FOR EACH ROW
-EXECUTE FUNCTION row_counts_trigger();
+        FOR EACH ROW
+        EXECUTE FUNCTION tx_cache_row_counts_trigger();
+END IF;
+END;
+$$;
+
+INSERT INTO row_counts (id, current_count)
+SELECT 'tx_cache', COUNT(*) FROM tx_cache;
