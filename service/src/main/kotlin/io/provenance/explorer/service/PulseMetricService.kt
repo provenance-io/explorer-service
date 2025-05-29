7e4786ea639be622fb113fa79c3620cbc4091a5f
Justin Forseth <jforseth@figuremarkets.com>
2025-05-29 09:22:56 -0600

    Define `THIRTY_DAYS` constant

--- service/src/main/kotlin/io/provenance/explorer/service/PulseMetricService.kt
+++ service/src/main/kotlin/io/provenance/explorer/service/PulseMetricService.kt
@@ -72,6 +72,7 @@ class PulseMetricService(
 ) {
     companion object {
         private val isBackfillInProgress = AtomicBoolean(false)
+        private const val THIRTY_DAYS = 30L
     }
 
     protected val logger = logger(PulseMetricService::class)
@@ -564,7 +565,7 @@ class PulseMetricService(
             atDateTime = atDateTime
         ) {
             val countForDates = TxCacheRecord.countForDates(
-                daysPrior = 30,
+                daysPrior = THIRTY_DAYS.toInt(),
                 atDateTime = atDateTime
             )
             val series = MetricSeries(
@@ -655,7 +656,7 @@ class PulseMetricService(
             range = range,
             atDateTime = atDateTime
         ) {
-            val days = 30L
+            val days = THIRTY_DAYS
             val startDate = nowUTC().minusDays(days).toLocalDate()
 
             val rangeSpan = rangeSpanFromCache(startDate, PulseCacheType.PULSE_PARTICIPANTS_METRIC, days)
