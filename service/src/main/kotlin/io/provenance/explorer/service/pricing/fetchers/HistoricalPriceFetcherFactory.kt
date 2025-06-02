6572bc413f523c5341695061dda4b6dc1c401a86
Eric Thompson <eric@figure.com>
2025-06-02 14:30:58 -0600

    Add CoinGeckoPriceFetcher, and use it for latest token cache and daily token data

--- service/src/main/kotlin/io/provenance/explorer/service/pricing/fetchers/HistoricalPriceFetcherFactory.kt
+++ service/src/main/kotlin/io/provenance/explorer/service/pricing/fetchers/HistoricalPriceFetcherFactory.kt
@@ -1,12 +1,9 @@
 package io.provenance.explorer.service.pricing.fetchers
 
-import io.provenance.explorer.config.ExplorerProperties.Companion.UTILITY_TOKEN
-
 class HistoricalPriceFetcherFactory() {
     fun createNhashPricingFetchers(): List<HistoricalPriceFetcher> {
         return listOf(
-            OsmosisPriceFetcher(),
-            NavEventPriceFetcher(UTILITY_TOKEN, listOf("uusd.trading", "uusdc.figure.se", "uusdt.figure.se"))
+            CoinGeckoPriceFetcher(),
         )
     }
 }
