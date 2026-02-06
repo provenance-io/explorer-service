68c9c148fd2aa6243b1900bc13b4b8955b1b2f7b
Eric Thompson <eric@figure.com>
2026-02-06 15:50:48 -0700

    Adding committed amounts to pulse asset summary

--- service/src/main/kotlin/io/provenance/explorer/grpc/v1/ExchangeGrpcClient.kt
+++ service/src/main/kotlin/io/provenance/explorer/grpc/v1/ExchangeGrpcClient.kt
@@ -1,6 +1,6 @@
 package io.provenance.explorer.grpc.v1
 
-import cosmos.base.query.v1beta1.Pagination
+import cosmos.base.query.v1beta1.pageRequest
 import io.grpc.ManagedChannelBuilder
 import io.provenance.exchange.v1.AccountAmount
 import io.provenance.exchange.v1.Market
@@ -10,6 +10,7 @@ import io.provenance.exchange.v1.QueryGetMarketCommitmentsRequest
 import io.provenance.exchange.v1.QueryGetMarketRequest
 import io.provenance.exchange.v1.QueryGrpcKt
 import io.provenance.explorer.config.interceptor.GrpcLoggingInterceptor
+import io.provenance.explorer.domain.extensions.toByteString
 import io.provenance.explorer.grpc.extensions.addBlockHeightToQuery
 import org.springframework.stereotype.Component
 import java.net.URI
@@ -55,17 +56,32 @@ class ExchangeGrpcClient(channelUri: URI) {
         } != null
     }
 
-    suspend fun getMarketCommitments(marketId: Int, height: Int? = null): List<AccountAmount> = exchangeClient
-        .addBlockHeightToQuery(height)
-        .getMarketCommitments(
-        QueryGetMarketCommitmentsRequest.newBuilder().apply {
-            this.marketId = marketId
-            // TODO cripes this is going to need paging
-            this.pagination = Pagination.PageRequest.newBuilder().apply {
-                limit = 10000
-            }.build()
-        }.build()
-    ).commitmentsList
+    suspend fun getMarketCommitments(marketId: Int, height: Int? = null): List<AccountAmount> {
+        val limit = 1000
+        var nextKey = "".toByteString()
+        val allCommitments = mutableListOf<AccountAmount>()
+
+        do {
+            exchangeClient
+                .addBlockHeightToQuery(height)
+                .getMarketCommitments(
+                    QueryGetMarketCommitmentsRequest.newBuilder().apply {
+                        this.marketId = marketId
+                        this.pagination = pageRequest {
+                            this.limit = limit.toLong()
+                            if (nextKey.toStringUtf8().isNotBlank()) {
+                                this.key = nextKey
+                            }
+                        }
+                    }.build()
+                ).let {
+                    nextKey = it.pagination.nextKey
+                    allCommitments.addAll(it.commitmentsList)
+                }
+        } while (nextKey.toStringUtf8().isNotBlank())
+
+        return allCommitments
+    }
 
     suspend fun totalCommitmentCount(height: Int? = null) = exchangeClient
         .addBlockHeightToQuery(height)
