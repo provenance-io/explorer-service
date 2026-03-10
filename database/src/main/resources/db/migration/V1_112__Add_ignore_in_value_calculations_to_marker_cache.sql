f066eeb955741a3c4e54795b21cf4b10f599ea04
Eric Thompson <eric@figure.com>
2026-03-10 10:48:25 -0600

    remove dup migration

--- database/src/main/resources/db/migration/V1_112__Add_ignore_in_value_calculations_to_marker_cache.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'Add ignore_in_value_calculations column to marker_cache' AS comment;
-
-ALTER TABLE marker_cache
-    ADD COLUMN IF NOT EXISTS ignore_in_value_calculations BOOLEAN NOT NULL DEFAULT FALSE;
-
-CREATE INDEX IF NOT EXISTS marker_cache_ignore_in_value_calculations_idx 
-    ON marker_cache(ignore_in_value_calculations) 
-    WHERE ignore_in_value_calculations = TRUE;
